cda7bef063acf77bc128bca0a602f6f7a43fa31f
dieramirezma <dieramirezma@unal.edu.co>
2024-10-18 01:04:48 -0500

    feat: Add CsvModule and CsvService logic for load csv and search products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatbotModule } from './chatbot/chatbot.module';
+import { CsvModule } from './csv/csv.module';
+import { CurrencyService } from './currency/currency.service';
+import { CurrencyModule } from './currency/currency.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), ChatbotModule, CsvModule, CurrencyModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CurrencyService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/csv/csv.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from 'src/interfaces/product.interface';
+import * as fs from 'fs';
+import * as csv from 'csv-parser';
+
+@Injectable()
+export class CsvService {
+  private products: Product[] = [];
+
+  constructor() {
+    this.transformCsv();
+  }
+
+  /*
+   * This method reads the csv file and transforms it into an array of Product objects
+  */
+  private transformCsv(): void {
+    fs.createReadStream('data/products_list.csv')
+      .pipe(csv())
+      .on('data', (row: Product) => this.products.push(row))
+      .on('end', () => {
+        console.log('Finish transforming csv file');
+      })
+  }
+
+  /*
+   * This method searches for products based on a keyword.
+   * keyword is a parameter given by the AI chatbot
+  */
+  searchProducts(keyword: string): Product[] {
+    // Convert the keyword to lowercase for easier search
+    const lowerCaseKeyword = keyword.toLowerCase();
+
+    /*
+     * Filter the products array based on the keyword
+     * Only return products that contain the keyword in the following fields:
+     * displayTitle, embeddingText and productType
+    */
+
+    const filteredProducts = this.products.filter((product) => {
+      return (
+        product.displayTitle.toLowerCase().includes(lowerCaseKeyword) ||
+        product.embeddingText.toLowerCase().includes(lowerCaseKeyword) ||
+        product.productType.toLowerCase().includes(lowerCaseKeyword)
+      )
+    })
+
+    return [filteredProducts[0], filteredProducts[1]];
+  }
+}
