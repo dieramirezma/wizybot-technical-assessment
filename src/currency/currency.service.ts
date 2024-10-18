b3d8c030b477b74583a690b2d20efcbfead9ffba
dieramirezma <dieramirezma@unal.edu.co>
2024-10-18 00:14:45 -0500

    feat: Implement CurrencyModule and CurrencyService logic

---
+++ src/currency/currency.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { ExchangeRates } from 'src/interfaces/exchange-rates.interface';
+
+@Injectable()
+export class CurrencyService {
+  // Get the latest exchange rates from the Open Exchange Rates API as an object
+  async getLatestExchangeRates(): Promise<ExchangeRates> {
+    try {
+      const response = await fetch(`https://openexchangerates.org/api/latest.json?app_id=${process.env.OPEN_EXCHANGE_RATES_APP_ID}`)
+
+      const data = await response.json()
+
+      // Return only the rates object
+      return data.rates
+    } catch (error) {
+      console.error('Error:', error)
+      return {};
+    }
+  }
+
+  // Convert currencies
+  async convertCurrencies(value: number, fromCurrency: string, toCurrency: string): Promise<number | undefined> {
+    // Get the latest exchange rates as an object
+    const exchangeRates: ExchangeRates = await this.getLatestExchangeRates()
+
+    if (exchangeRates[fromCurrency] && exchangeRates[toCurrency]) {
+      // Get the exchange rate for the two currencies
+      const exchangeRate = Number(exchangeRates[toCurrency]) / Number(exchangeRates[fromCurrency])
+      return value * exchangeRate
+    }
+  }
+}
---
+++ src/interfaces/exchange-rates.interface.ts
@@ -0,0 +1,3 @@
+export interface ExchangeRates {
+  [currencyCode: string]: number;
+}
